ea7a1aa9cac034a0ad90cf10eb81a8e9df6582fa
Brad Erickson <20berickson@gmail.com>
2026-01-19 18:31:25 -0700

    Correct Navbar layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,27 @@
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <h1 className="navbar-title">My Application</h1>
-      <ul className="navbar-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
+    <>
+        {/* TODO: Add custom gray */}
+        <nav className="navbar bg-gray-300 p-4 text-black flex justify-between items-center">
+
+        {/* TODO: Add logo icon */}
+        <div className="navbar-brand">
+            <h1 className="navbar-title">DocRelief AI</h1>
+        </div>
+        
+        <div className="navbar-menu">
+            <ul className="navbar-links flex gap-4">
+            <li><a href="/">Features</a></li>
+            <li><a href="/about">Pricing</a></li>
+            <li className="ml-8"><a href="/contact">Login/Username</a></li>
+        </ul>
+        </div>
+
+        
+
+        
+        </nav>
+    </>
+    
   );
 }
