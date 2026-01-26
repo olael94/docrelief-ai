1551c1a83ea19c23d0171fe56e6e06aa762dc3d5
Brad Erickson <20berickson@gmail.com>
2026-01-25 18:26:54 -0700

    Fix PublicRepoTab formatting

--- frontend/src/components/PublicRepoTab.jsx
+++ frontend/src/components/PublicRepoTab.jsx
@@ -3,9 +3,10 @@ import HeroButton from "./HeroButton";
 export default function PublicRepoTab() {
     return (
         <div className="flex flex-col items-center justify-center min-w-125">
-            <input className="bg-navbar rounded-3xl p-2 w-full text-center" type="text" placeholder="Paste your public repo https://github.com/username/repo"></input>
+            <h1 className="self-start pt-4 pb-4">Repository URL</h1>
+            <input className="bg-navbar rounded-3xl p-2 w-full text-center" type="text" placeholder="https://github.com/username/repo"></input>
                 <HeroButton text="Generate README →" />
-            <span className="mt-4 mb-4 text-bold">or upload files directly</span>
+            <span className="mt-4 mb-4 text-bold">Use this tab for public Github Repositories</span>
         </div>
     );
 }
