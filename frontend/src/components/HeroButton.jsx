df57af236c8aa82bb16aa725bf26d5290e5cad66
Brad Erickson <20berickson@gmail.com>
2026-01-25 18:27:20 -0700

    Add specifiable width to HeroButton

--- frontend/src/components/HeroButton.jsx
+++ frontend/src/components/HeroButton.jsx
@@ -1,7 +1,7 @@
-export default function HeroButton({ text, onClick }) {
+export default function HeroButton({ text, onClick, width }) {
     return (
         <button
-            className="btn-landing text-white pt-3 pb-3 pl-5 pr-5 rounded-3xl mt-4"
+            className={`btn-landing text-white pt-3 pb-3 pl-5 pr-5 rounded-3xl mt-4 ${width || ''}`}
             onClick={onClick}
         >
             {text}
