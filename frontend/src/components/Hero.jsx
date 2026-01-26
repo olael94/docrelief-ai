7672872b4b5fe1e3e2f184908a85d208ae2a93e3
Brad Erickson <20berickson@gmail.com>
2026-01-25 18:27:34 -0700

    Add instruction cards to Hero Section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,6 +4,7 @@ import TabBar from "./TabBar"
 import PublicRepoTab from "./PublicRepoTab";
 import UploadTab from "./UploadTab";
 import PrivateRepoTab from "./PrivateRepoTab";
+import HeroCard from "./HeroCard";
 
 export default function Hero() {
     const [activeTab, setActiveTab] = useState('public-repo');
@@ -27,12 +28,19 @@ export default function Hero() {
             <div className="pl-5 pr-5 rounded-4xl h-150 w-200 shadow-2xl flex flex-col items-center justify-center bg-white gap-4">
                 <h1 className="font-poppins text-4xl font-bold ">Generate README</h1>
                 <TabBar activeTab={activeTab} setActiveTab={setActiveTab} tabs={tabs} />
-                <div>
+                <div className="h-100">
                     {activeTab === 'public-repo' && <PublicRepoTab />}
                     {activeTab === 'upload-files' && <UploadTab />}
                     {activeTab === 'private-repo' && <PrivateRepoTab />}
                 </div>
-            </div>    
+            </div>
+            <div className="mt-30">
+                <h1 className="text-2xl">Three ways to generate your perfect README</h1>
+                <HeroCard title={"Public Repository"} text={"Paste any public GitHub URL. \nNo login required"} buttonText={"Generate README → "} width="min-w-32"/>
+                <HeroCard title={"Upload Code Files"} text={"Drag & drop your .py, .js, .java files. Works without GitHub"} buttonText={"Upload → "} width="min-w-100"/>
+                <HeroCard title={"Private Repository"} text={"Connect GitHub to access private repos\nCommit README directly to your repo"} buttonText={"Get Connect → "} width="min-w-32"/>
+            </div>  
+            
         </div>
         </>
     )
