cb2c7b7998fc2ea48b850be24656311b28050476
olael94 <104049815+olael94@users.noreply.github.com>
2026-02-03 01:49:16 -0700

    fix/ Container behavior from UploadTab, PrivateRepoTab.
    - Adjusted drop zone for files to accept Zip files only. (wording only)
    _ Added mobile responsiveness for all three tabs.

--- frontend/src/components/UploadTab.jsx
+++ frontend/src/components/UploadTab.jsx
@@ -1,25 +1,26 @@
 import HeroButton from "./HeroButton";
+import {FolderOpen} from "lucide-react";
 
 export default function UploadTab() {
     return (
         <>
-            <div className="w-full flex flex-col items-center justify-center p-4">
-                <div className="bg-gray-200 rounded-3xl">
-                    <div className="p-6 flex flex-col items-center justify-center">
-                        <p>+</p>
-                        <p className="pb-2">Drag & drop files here or click to browse</p>
-                        <p>Supported: .py .js . java .tsx .cpp .go .rs</p>
-                        <p>Max: 50 files * 10MB total</p>
+            <div className="w-[340px] md:w-[660px] flex flex-col items-center h-full min-h-[500px]">
+                <div className="bg-gray-100 rounded-3xl w-full h-[200px]">
+                    <div className="p-9 flex flex-col items-center w-full justify-center">
+                        <FolderOpen className="text-gray-400 w-10 h-10"/>
+                        <p className="pb-2 text-gray-400">Drag & drop your zip file here or click to browse</p>
+                        <p className="text-gray-400">Supported: .zip files only</p>
+                        <p className="text-gray-400">Max: 10MB</p>
                     </div>
                 </div>
 
-                <h1 className="self-start pt-4 pb-4">Uploaded files (0)</h1>
+                <h1 className="self-start pt-4 pb-4 font-fire-code text-bold">Uploaded files (0)</h1>
                 <ul>
-                    <li>No files uploaded yet.</li>
+                    <li className="self-start pt-4 pb-4 font-fire-code text-bold">No files uploaded yet.</li>
                 </ul>
-
-                <HeroButton text="Generate README from Files →"/>
-
+                <div className="mt-auto mb-9">
+                    <HeroButton text="Generate README from Files →"/>
+                </div>
                 {/* <input type="file" onChange={handleFileChange}></input> */}
 
             </div>
